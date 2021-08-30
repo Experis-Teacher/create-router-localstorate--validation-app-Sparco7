73498c8285f5f8d5441ff6e73099609790bcd5b3
Barak Ben Gur <barakbbg@gmail.com>
2021-08-30 12:56:51 +0300

    initail validation

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,16 +3,39 @@ import React, { Component } from "react";
 export default class About extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({name: e.target.value})
   }
 
   saveName(e) {
-    localStorage["name"] = e.target.value;
+    if (e.target.value) {
+      console.log(e.target.value);
+      // this.setState({ name: e.target.value });
+      localStorage["name"] = e.target.value;
+    }
   }
 
   navigate = () => {
-    this.props.history.push("/contact");
-  }
+    
+    if (this.isValidName(this.state.name)) {
+      console.log("valid");
+      localStorage.setItem("name", JSON.stringify(this.state.name));
+      this.props.history.push("/contact");
+    } else {
+      console.log("not valid");
+    }
+  };
+
+// checks if the full name is valid
+isValidName = (name) => {
+  var pattern = /^[a-zA-Z ]+$/;
+  return pattern.test(name);
+}
 
   render() {
     return (
@@ -22,7 +45,9 @@ export default class About extends Component {
           type="text"
           placeholder="Enter your name"
           className="name"
-          onBlur={this.saveName}
+          onChange={this.handleChange}
+          // onChange={this.saveName}
+          value={this.state.name}
         />
         <button onClick={this.navigate}>navigate</button>
       </div>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,26 +3,48 @@ import React, { Component } from "react";
 export default class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      phone: '',
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({ phone: e.target.value });
+  };
 
   savePhone(e) {
     localStorage["phone"] = e.target.value;
   }
 
-  navigate  = () => {
-    this.props.history.push("/finish");
-  }
+  navigate = () => {
+    if (this.isPhoneValid(this.state.phone)) {
+      console.log("valid");
+      localStorage.setItem("phone", JSON.stringify(this.state.phone));
+      this.props.history.push("/finish");
+    } else {
+      console.log("not valid");
+    }
+  };
+
+ // checks if the phone is valid
+isPhoneValid = (phone) => {
+  var pattern = /[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}/g;
+  // var pattern = /[0-9]{6}/g;
+
+  return pattern.test(phone) || phone == "";
+}
 
   render() {
     return (
       <div>
-        <h1> About</h1>
+        <h1> Contact</h1>
         <input
-          type="text"
+          type="phone"
           placeholder="Enter your phone"
           className="phone"
-          onBlur={this.savePhone}
+          // onBlur={this.savePhone}
+          value={this.state.phone}
+          onChange={this.handleChange}
         />
         <button onClick={this.navigate}>navigate</button>
       </div>
